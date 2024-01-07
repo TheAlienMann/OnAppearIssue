1498fe0036f705db918cb018c20d5ebf326ab457
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-01-07 14:48:19 +0330

    add Volodymyr's code.

--- OnAppearIssue/ContentView.swift
+++ OnAppearIssue/ContentView.swift
@@ -1,24 +1,75 @@
 //
 //  ContentView.swift
-//  OnAppearIssue
-//
-//  Created by Mohamad Rahmani on 07.01.24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  @State var isLoggedIn: Bool = false
+  
+  var body: some View {
+    if isLoggedIn {
+      TabView {
+        TabViewView(title: "Store", tabIcon: "trash", isLoggedIn: $isLoggedIn)
+        TabViewView(title: "New", tabIcon: "newspaper", isLoggedIn: $isLoggedIn)
+        TabViewView(title: "Gallery", tabIcon: "photo", isLoggedIn: $isLoggedIn)
+        TabViewView(title: "Discover", tabIcon: "magnifyingglass", isLoggedIn: $isLoggedIn)
+        TabViewView(title: "Profile", tabIcon: "person", isLoggedIn: $isLoggedIn)
+      }
+    } else {
+      LoginView {
+        isLoggedIn = true
+      }
     }
+  }
 }
 
-#Preview {
-    ContentView()
+struct TabViewView: View {
+  let title: String
+  let tabIcon: String
+  @Binding var isLoggedIn: Bool
+  
+  var body: some View {
+    NavigationStack {
+      Text(title)
+        .navigationBarTitle(title)
+        .task {
+          print("🏋️ \(title) some task")
+        }
+        .onAppear {
+          print("👋 \(title) onAppear")
+        }
+        .onDisappear {
+          print("😶‍🌫️ \(title) onDisappear")
+        }
+        .toolbar {
+          ToolbarItem(placement: .navigationBarTrailing) {
+            Button("Logout") {
+              isLoggedIn = false
+            }
+          }
+        }
+    }
+    .task {
+      print("🏋️ \(title) NavigationStack some task")
+    }
+    .onAppear {
+      print("👋 \(title) NavigationStack onAppear")
+    }
+    .onDisappear {
+      print("😶‍🌫️ \(title) NavigationStack onDisappear")
+    }
+    .tabItem {
+      Image(systemName: tabIcon)
+      Text(title)
+    }
+  }
+}
+
+struct LoginView: View {
+  let onLogin: () -> Void
+  
+  var body: some View {
+    Button("Login", action: onLogin)
+  }
 }
