b539ec1fa6cbc4966a003f973dbd401d3d9bd3b5
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-01-07 14:17:52 +0330

    Initial Commit

---
+++ OnAppearIssue/OnAppearIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnAppearIssueApp.swift
+//  OnAppearIssue
+//
+//  Created by Mohamad Rahmani on 07.01.24.
+//
+
+import SwiftUI
+
+@main
+struct OnAppearIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
